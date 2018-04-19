1b639d76e39c222904f1bb673ac23e10444f0714
sungeun0528 <30641853+sungeun0528@users.noreply.github.com>
2018-04-19 15:20:22 +0900

    04.19

--- src/main/scala/com/kopochaeproject/Example_Rdd.scala
+++ src/main/scala/com/kopochaeproject/Example_Rdd.scala
@@ -1,8 +1,7 @@
 package com.kopochaeproject
 import org.apache.spark.sql.SparkSession;
 
-
-object Example_JoinFromOracle {
+class Example_Rdd {
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder().appName("hkProject").
@@ -61,7 +60,7 @@ object Example_JoinFromOracle {
     var productnameNo = rawDataColumns.indexOf("PRODUCTNAME")
 
 
-   // 컬럼 배열확인방법 (QTY가 4번째에 있는 것을 확인)
+    // 컬럼 배열확인방법 (QTY가 4번째에 있는 것을 확인)
     rawDataColumns.indexOf("QTY")
 
     //컬럼명이 들어있는 지 확인할 때, 먼저 Set으로 만들어준다.
@@ -74,100 +73,28 @@ object Example_JoinFromOracle {
     //컬럼명이 바뀌고 생성된 데이터를 볼 수 있음.
     testDf.show(1)
 
-    //대소문자상관없이 같은지 확인하고 싶을 때,
-    //첫번째 방법
-    var test1 = "aabbCCDD"
-    var test2 = "AABBCCdd"
-    test1.equalsIgnoreCase(test2)
-    //두번째 방법 (둘다 소문자로 바꾸고 확인하는 방법)
-    var a = test1.toLowerCase()
-    var b = test2.toLowerCase()
-    a== b
-
-
-    // 데이터 저장 ( 데이터 베이스 안에 저장할 때)
-
-    var myUrl = "jdbc:oracle:thin:@127.0.0.1:1522/XE"
-
-    val prop = new java.util.Properties
-    prop.setProperty("driver", "oracle.jdbc.OracleDriver")
-    prop.setProperty("user", "haiteam")
-    prop.setProperty("password", "haiteam")
-    val table = "hk_join"
-    //append
-    resultDf.write.mode("overwrite").jdbc(myUrl, table, prop)
-
-    // Dataframe.toDf.....
-    ///////////// Oracle Express Usage ////////////////
-    //alter system set processes=500 scope=spfile
-    //show parameter processes
-    //shutdown immediate
-    //startup
-
-   //데이터 저장 (컴퓨터 안에 저장할 때)
-    rawData.
-      coalesce(1). // 파일개수
-      write.format("csv").  // 저장포맷
-      mode("overwrite"). // 저장모드 append/overwrite
-      option("charset", "ISO-8859-1").
-      option("header", "true"). // 헤더 유/무
-      save("e:/resultdf2.csv")
-    //ISO-8859-1 ISO-8859-1
-
-    //데이터 저장 (rdd파일로 컴퓨터 안에 저장할 때)
-    rawData.rdd.coalesce(1).map { x =>x.mkString(",") }.saveAsTextFile("e:/test/test3.csv")
-
-    //RDD 변환
     var rawRdd = rawData.rdd
 
-    //RDD 처리(데이터 정제)
-    //(KECOL, ACCOUNTID, PRODUCT, YEARWEEK, QTY, PRODUCTNAME)
-    var filterexRdd = rawRdd.filter(x=> {
-      // 데이터 한줄씩 들어옴
+    //필터예제 : 자신이 생성한 Rdd에 연주차 정보가 52보다 큰 값은 제거하는 로직직
+   var filteredRdd = rawRdd.filter(x => {
+      //boolean = true
       var checkValid = true
-      // 그중 특정 컬럼값
-      var yearweek = x.getString(yearweekNo)
+      // 찾기 : yearweek 인덱스로 주차정보만 인트타입으로 변환
+      var weekValue = x.getString(yearweekNo).substring(4).toInt
 
-      if(yearweek.length != 6){
+      // 비교한 후 주차정보가 53 이상인 경우 레코드 삭제
+      if (weekValue >= 53) {
         checkValid = false
       }
-      //if(x.getString(yearweekNo).length != 6) {checkValid = false}
+
       checkValid
     })
 
-
-    //디버깅 방법 (case1)
-    var filterexRdd1 = rawRdd
-    var x = filterexRdd1.first
-
-
-    //디버깅 방법 (case2)
-    var filterex2Rdd = rawRdd.filter(x1=>{
-      (x1.getString(yearweekNo) == "201512")
-    })
-    var x1 = filterex2Rdd.first
-
-    var filterex3Rdd = rawRdd.filter(x2=>{
-      var checkValid = false
-      if((x2.getString(accountidNo) =="A60") &&
-        (x2.getString(productNo) == "PRODUCT34") &&
-        (x2.getString(yearweekNo) == "201402")){
-        checkValid = true
-      }
-      checkValid
-    })
-    var x2 = filterex3Rdd.first
-
-    //자신이 생성한 Rdd에 연주차 정보가 52보다 큰 값은 제거하는 로직 구현
-    var filterex4Rdd = rawRdd.filter(x3=>{
-      var checkValid = true
-      if (x3.getString(yearweekNo).substring(4,2) > "52" ){
-        checkValid = false
-      }
-      checkValid
-    })
-    var x3 = filterex4Rdd.first
-
+    //데이터 조회하고 싶을 때,
+    //3개만 불러올 때
+    filteredRdd.take(3).foreach(println)
+    //다 불러 올때
+    filteredRdd.collect.toArray.foreach(println)
 
     //필터예제2 : 상품정보가 PRODUCT1,2 인 정보만 필터링 하세요.
     //분석대상 제품군 등록
@@ -175,25 +102,23 @@ object Example_JoinFromOracle {
     //세트 타입으로 변환
     var productSet = productArray.toSet
 
-    var resultRdd = rawRdd.filter(x4=>{
+    var resultRdd = filteredRdd.filter(x=>{
       var checkValid = false
 
       //데이터 특정 행의 product 컬럼인덱스를 활용하여 데이터 대입
-      var productInfo = x4.getString(productNo)
+      var productInfo = x.getString(productNo)
 
       if(productSet.contains(productInfo)){
         checkValid = true
       }
+      //2번째 정답
+      if((productInfo == "PRODUCT1")||
+        (productInfo == "PRODUCT2")){
+        checkValid = true
+      }
       checkValid
     })
-    var x4 = resultRdd.first
-
-
-    //필터된 데이터 갯수를 확인하고 싶을 때,
-    filterexRdd.count()
-
-
-
 
   }
+
 }

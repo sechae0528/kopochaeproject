beafa996a944e931517c7fee459fb7c3a8137a71
sungeun0528 <30641853+sungeun0528@users.noreply.github.com>
2018-04-20 10:52:37 +0900

    04.20

--- src/main/scala/com/kopochaeproject/Example_Rdd.scala
+++ src/main/scala/com/kopochaeproject/Example_Rdd.scala
@@ -1,7 +1,7 @@
 package com.kopochaeproject
 
 import org.apache.spark.sql.SparkSession
-
+import org.apache.spark.sql.Row
 //Rdd에서 Dataframe으로 변환시 타입 import 해주기!!
 import org.apache.spark.sql.types.{StringType, DoubleType, StructField, StructType}
 
@@ -85,7 +85,12 @@ object Example_Rdd {
     var rawRdd = rawData.rdd
 
 
-    //////////////////////////////RDD로 변환 후 정제하기 ///////////////////////////
+    //////////////////////////////RDD로 변환 후 정제하기(filter) ////////////////////////
+    // 데이터형 변환 [데이터프레임 → RDD]
+    //var {RDD변수명} = {RDD변수명}.filter(x=>{ 필터조건식})
+    // 데이터 확인
+    //var {RDD변수명}.collect.foreach(println)
+
     //필터예제 : 자신이 생성한 Rdd에 연주차 정보가 52보다 큰 값은 제거하는 로직직
     var filteredRdd = rawRdd.filter(x => {
       //boolean = true
@@ -101,7 +106,7 @@ object Example_Rdd {
       checkValid
     })
 
-    //데이터 조회하고 싶을 때,
+    ////데이터 조회하고 싶을 때/////////////////////////////////
     //3개만 불러올 때
     filteredRdd.take(3).foreach(println)
     //다 불러 올때
@@ -123,10 +128,10 @@ object Example_Rdd {
         checkValid = true
       }
       //2번째 정답
-      if((productInfo == "PRODUCT1")||
-        (productInfo == "PRODUCT2")){
-        checkValid = true
-      }
+//      if((productInfo == "PRODUCT1")||
+//        (productInfo == "PRODUCT2")){
+//        checkValid = true
+//      }
       checkValid
     })
 
@@ -144,6 +149,61 @@ object Example_Rdd {
       StructField("QTY", DoubleType),
       StructField("PRODUCTNAME", StringType))))
 
+    //////////////////////Rdd가공 : map  (Rdd정제 : filter)//////////////////////////////////////
+    // 데이터형 변환 [데이터프레임 → RDD]
+    //var {RDD변수명} = {RDD변수명}.map(x=>{ 컬럼정보 수정
+     // Row( 컬럼, 컬럼          )        })
+    // 데이터 확인
+    //var {RDD변수명}.collect.foreach(println)
+
+    ///////////////Row로 로직만든 map(이게 많이 쓰임!!!)//////////////////
+
+    //처리로직 : MAXVALUE 이상인 건은 MAXVALUE로 치환한다.
+    var MAXVALUE = 700000
+
+    var mapRdd = rawRdd.map(x=>{
+      //디버깅코드 : var x = mapRdd.filter(x=>{x.getDouble(qtyNo) > 700000 }).first
+      //로직구현예정
+      var org_qty = x.getDouble(qtyNo)
+      var new_qty = org_qty
+
+      if(new_qty > MAXVALUE) {
+        new_qty = MAXVALUE
+      }
+      //출력 row 키정보, 거래량 정보, 거래량 정보_new)
+      Row( x.getString(keyNo),
+        x.getString(yearweekNo),
+        org_qty,
+        new_qty)
+
+    })
+
+
+    //////////////////일반적으로 로직만든 map(많이쓰지않음)////////////////////
+    //////인덱스가 설정되있지않아 컬럼정보의 자리를 바꿀 수 없다./////////////
+    var map1Rdd = rawRdd.map(x=>{
+      var org_qty = x.getDouble(qtyNo)
+      var new_qty = org_qty
+
+      if(new_qty > MAXVALUE){new_qty = MAXVALUE}
+      ( x.getString(keyNo),
+        x.getString(yearweekNo),
+        org_qty,
+        new_qty)
+      //x.getString(qtyNo) ==>기존값
+    })
+    mapRdd.collect.toArray.foreach(println)
+
+
+    // map 디버깅 첫번째방법
+    var mapexRdd = rawRdd
+    var x = mapexRdd.first
+
+    //map 디버깅 두번째방법
+    var mapex2Rdd = rawRdd
+    var x2 = mapex2Rdd.filter(x=> {
+      (x.getDouble(qtyNo).toDouble > 700000)
+    }).first
 
 
 
